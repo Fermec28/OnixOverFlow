44a4babc1579382567255d6f64e7b13767b2afd0
Fermedina <fermed28@gmai.com>
2017-12-26 20:48:17 -0500

    change subject to mail

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -6,7 +6,7 @@ class QuestionMailer < ApplicationMailer
     	mg_client = Mailgun::Client.new ENV['MAIL_GUN_KEY']
     	message_params = {:from    => 'info@OnixOverflow.com',
                       :to      => @question.user.email,
-                      :subject => 'New Question on  OnixOverFlow',                      
+                      :subject => @question.title,                      
                       :html    =>  text.to_str}
     	mg_client.send_message ENV['MAIL_GUN_DOMAIN'], message_params    	
   	end
